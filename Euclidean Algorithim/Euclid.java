7988af9497456b502be1b22afcc9f01c1d26faf6
Jamie Vasales <jamie.vasales@outlook.com>
2019-02-09 17:57:03 +1100

    A new version of the class is not needed when the function is static. Updated

--- Euclidean Algorithim/Euclid.java
+++ Euclidean Algorithim/Euclid.java
@@ -4,17 +4,17 @@ public class Euclid {
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        Euclid euclidian = new Euclid();
+        //Euclid euclidian = new Euclid();
         int num1,num2;
         System.out.println("Please enter two numbers: Biggest to smallest respectively \nPlease hit enter after each number");
         num1 = keyboard.nextInt();
         num2 = keyboard.nextInt();
         keyboard.close();
-        System.out.println("the greatest common divisor is " + Integer.toString(euclidian.euclid(num1,num2)));
+        System.out.println("the greatest common divisor is " + Integer.toString(euclid(num1,num2)));
     }
 
 
-        public int euclid(int num1, int num2) {
+        public static int euclid(int num1, int num2) {
             if(num2==1)
             return 1;
             else if(num1%num2==0) {
