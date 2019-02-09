18cf017b9fe44196d90715ab10472de59d2d1ae4
Jamie Vasales <jamie.vasales@outlook.com>
2019-02-09 18:05:28 +1100

    Catch exceptions for useful feedback on non integer

--- Euclidean Algorithim/Euclid.java
+++ Euclidean Algorithim/Euclid.java
@@ -5,10 +5,16 @@ public class Euclid {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         //Euclid euclidian = new Euclid();
-        int num1,num2;
-        System.out.println("Please enter two numbers: Biggest to smallest respectively \nPlease hit enter after each number");
+        int num1=0,num2=0;
+        System.out.println("Please enter two numbers: Please hit enter after each number");
+        try {
         num1 = keyboard.nextInt();
         num2 = keyboard.nextInt();
+        }
+        catch(Exception e) { //Exit gracefully with useful feedback
+            System.out.println("Please enter two integers. Please check input before submitting");
+            System.exit(0);
+        }
         keyboard.close();
         System.out.println("the greatest common divisor is " + Integer.toString(euclid(num1,num2)));
     }
